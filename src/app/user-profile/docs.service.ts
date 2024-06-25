1319bcbfb9557f5eea44e9a6cc84b8567eb3bd1d
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2024-06-24 23:55:43 -0300

    Fetch all docs

--- src/app/user-profile/docs.service.ts
+++ src/app/user-profile/docs.service.ts
@@ -1,10 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHandlerFn } from '@angular/common/http';
+
+const BASE_URL = 'http://localhost:3000/docs';
+
+export type Doc = {
+  id: string;
+  title: string;
+  author: string;
+  description: string;
+  publish_date: Date;
+};
 
 @Injectable({
   providedIn: 'root',
 })
 export class DocsService {
+  constructor(private http: HttpClient) {}
   getDocs() {
-    return docs;
+    return this.http.get<Doc>(BASE_URL);
   }
 }
