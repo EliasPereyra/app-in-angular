92e9b6fd55b8180b39b18eb9e3fc7b234c35a769
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2024-07-02 23:05:11 -0300

    Update user profile

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Doc, DocsService } from './docs.service';
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
   imports: [],
   templateUrl: './user-profile.component.html',
-  styleUrl: './user-profile.component.css'
+  styleUrl: './user-profile.component.css',
 })
 export class UserProfileComponent {
+  doc$!: Observable<Doc>;
 
+  constructor(private docServie: DocsService) {}
+
+  ngOnInit(): any {
+    this.doc$ = this.docServie.getDocs();
+  }
 }
