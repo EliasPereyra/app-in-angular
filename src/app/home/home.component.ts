7171e88a9fe074ee0b6462d61e9e98fb2434d8df
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2024-07-03 23:37:34 -0300

    Update the home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,8 +5,8 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-
+  title = 'See documents from the community';
 }
